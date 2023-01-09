6737ed4de6eb718d5d1c6071eb595bcd7158e7bc
Divine Obeten <divineobeten9@gmail.com>
2023-01-09 19:01:02 +0100

    manipulating the dom

---
+++ main.js
@@ -0,0 +1,4 @@
+const cardNumber = document.getElementsByClassName("card__number");
+const cardName = document.getElementsByClassName("card__name");
+const expMonth = document.getElementsByClassName("month");
+const expYear = document.getElementsByClassName("year");
