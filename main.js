04f6c50d932d5ef39c3a391be1b75fb99d8cda15
Divine Obeten <divineobeten9@gmail.com>
2023-01-17 07:26:28 +0100

    creating the hideform function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const form = document.getElementById("form");
+const Form = document.getElementById("form");
 const nameInput = document.getElementsByClassName("nameInput");
 const numberInput = document.getElementsByClassName("numberInput");
 const yearInput = document.getElementsByClassName("input__year");
@@ -12,6 +12,15 @@ const cvvErr = document.getElementById("cvverr");
 const cardNumberErr = document.getElementById("numbererr");
 const dateErr = document.getElementById("daterr");
 const nameErr = document.getElementById("nameerr");
-const confirmBtn = document.getElementsByClassName("confirm__btn");
-const complete = document.getElementById("complete");
+// const confirmBtn = document.getElementsByClassName("confirm__btn");
+const completeScreen = document.getElementById("complete");
 const continueBtn = document.getElementsByClassName("continue");
+
+function hideForm() {
+    // Form.style.display = "none";
+    // completeScreen.style.display = "flex"
+    // alert("hidden")
+
+    Form.classList.add("formHidden")
+    completeScreen.classList.add("showComplete")
+}
